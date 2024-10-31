e159eb562bb1924cdda5c742ef46c9395f230ee4
Bernard <benbaah104@gmail.com>
2024-10-31 01:56:18 -0700

    Kotlin inheritance

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,30 @@
+
+//Kotlin inheritance
+open class Car {
+    var Volt = "V7"
+    var Tyre = "leather"
+    open fun getInfo() {
+        println("Engine volt is $Volt and Tyre type is ${Tyre}")
+    }
+}
+
+class Sportcar: Car(){
+    var Height = 200
+}
+
+// Overriding Method
+class Salooncar: Car(){
+    override fun getInfo(){
+        println("hello Sallon")
+    }
+}
+
 fun main() {
     println("Hello World!")
+
+    // inheritance
+    var spH = Sportcar().Volt
+    println(spH)
+
+    Salooncar().getInfo()
 }
